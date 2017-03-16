166bed3879e49db48af5d9463dd1c4211ce00833
ptusa31 <ptusa31@yahoo.com>
2017-03-16 16:53:53 -0400

    ,

--- ConsoleApplication1/Class1.cs
+++ ConsoleApplication1/Class1.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication1
         int i = 0;
         string message = "Hello from class 1";
         string msg = "new message";
+        string msg1 = "new new message";
         
     }
 }
