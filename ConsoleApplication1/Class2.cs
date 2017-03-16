e6fe385782d66da47d43cea01b290ab8557a891a
PramodT <pramod@diaspark.com>
2017-03-16 16:06:12 -0400

    s

--- ConsoleApplication1/Class2.cs
+++ ConsoleApplication1/Class2.cs
@@ -3,11 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 namespace ConsoleApplication1
 {
     class Class2
     {
-
+        string msg = "Hello from class 2";
     }
 }
